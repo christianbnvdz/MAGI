5b15d118cf1411f98b16c4bcd4f767b87bcbe5c6
Christian Benavidez <christianbenavidez57@gmail.com>
2021-08-05 21:15:32 -0700

    Add on-message handler
    Assume that there is a prefix set in an .env file. Check to see if the
    message should be handled as a command and, if so, split up the message
    into a command name portion and an argument array portion.

--- magi.js
+++ magi.js
@@ -5,7 +5,14 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.once('ready', () => {
-    client.channels.cache.get('746086612697350228').send('Real Dyno Hours Worker');
+    //client.channels.cache.get('746086612697350228').send('Hello, I am back.');
+});
+
+client.on('message', message => {
+    if (!message.content.startsWith(process.env.PREFIX) || message.author.bot) return;
+
+    const args = message.content.slice(process.env.PREFIX.length).trim().split(/\s+/);
+    const command = args.shift().toLowerCase();
 });
 
 client.login(process.env.TOKEN);
