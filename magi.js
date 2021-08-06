98d6f18dae39457c01a26dd2d20de8c027b0db74
Christian Benavidez <christianbenavidez57@gmail.com>
2021-08-05 21:47:16 -0700

    Require command files, place in client collection
    Require all the command files in the commands directory and place them
    into the client's collection of commands to act as a dispatch with the
    name of the command being the key.

--- magi.js
+++ magi.js
@@ -1,8 +1,16 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
 
 client.once('ready', () => {
     //client.channels.cache.get('746086612697350228').send('Hello, I am back.');
