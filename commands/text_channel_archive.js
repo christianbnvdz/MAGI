ce5cefc4fa93fb726c21818d1077e2183da2746b
Christian Benavidez <christianbenavidez57@gmail.com>
2021-08-06 20:02:14 -0700

    Send metadata information about the guild, channel
    When the archive command is called, the bot will get all the messages in
    the channel and also get some select guild and channel information and
    store it into an object.
    
    The information is not sent still. Right now only some guild and channel
    info is retreived. No message info, besides the snowflakes, is gathered
    and placed into the object that holds all the information we are going
    to send.

--- commands/text_channel_archive.js
+++ commands/text_channel_archive.js
@@ -1,28 +1,60 @@
+const Discord = require('discord.js')
 const fs = require('fs');
 
 module.exports = {
 	name: 'archive',
 	execute(message, args) {
             const channel_as_json = scrape_channel(message.channel);
-	    console.log(channel_as_json);
 	},
 };
 
+// channel is of type TextChannel
 async function scrape_channel(channel) {
     // Only allowed to grab 100 messages at a time
     let messages = await channel.messages.fetch({limit: 100});
     let messages_retreived = messages.size;
     while (messages_retreived === 100) {
-        const last_message_snowflake = messages.lastKey();
-        const message_batch = await channel.messages.fetch({limit: 100, before: last_message_snowflake});
+        const message_batch = await channel.messages.fetch({limit: 100, before: messages.lastKey()});
 	messages = messages.concat(message_batch);
 	messages_retreived = message_batch.size;
     }
-    //fs.writeFile('test.json', JSON.stringify(messages), 'utf8', () => {});
-    /*await channel.send({
+
+    // Get general channel info
+    let channel_data = {
+	guild_id: channel.guild.id,
+	guild_name: channel.guild.name,
+	guild_description: channel.guild.description,
+	guild_creation_date: channel.guild.createdAt,
+	guild_owner_id: channel.guild.ownerID,
+        channel_id: channel.id,
+	channel_name: channel.name,
+	channel_topic: channel.topic,
+	channel_creation_date: channel.createdAt,
+	//channel_members:channel.members,        cant get this to work quite yet. Only cached members
+	channel_nsfw: channel.nsfw,
+	channel_message_count: 0,
+	channel_messages: new Discord.Collection(),
+    };
+
+    if (messages.size > 0) {
+	// Extract only the data we want from messages
+	messages.each((message) => {
+            let message_data = {
+                
+	    };
+	    // Use the message snowflake as the key
+	    channel_data.channel_messages.set(message.id, message_data);
+	});
+	// also store the number of messages in the channel
+	channel_data.channel_message_count = channel_data.channel_messages.size;
+    }
+    console.log(channel_data);
+
+    /*fs.writeFile('metadata.json', JSON.stringify(channel_data), 'utf8', () => {});
+    await channel.send({
 	files: [{
-	    attachment: './test.json',
-	    name: 'last_100_messages.json'
+	    attachment: './metadata.json',
+	    name: 'metadata.json'
 	}]
     });*/
 }
