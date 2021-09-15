8fe0279a8728172dd5847dc8bab48e47727e6144
Christian Benavidez <christianbenavidez57@gmail.com>
2021-09-14 17:06:19 -0700

    Replace comments in magi.js with jsdoc comments
    Also create a typedef in the utils/commands.js file to be able to
    specify the type taken by loadCommandType().

--- utils/command.js
+++ utils/command.js
@@ -1,6 +1,10 @@
 // Command types must be set to the name of the directory
 // that contains them. Ex: Admin commands are held in admin
 // so the type is set to admin
+/**
+ * The ADMIN, CHAT, or MISC command type
+ * @typedef {CommandType.ADMIN|CommandType.CHAT|CommandType.MISC} CommandType
+ */
 const CommandType = {
   ADMIN: 'admin',
   CHAT: 'chat',
